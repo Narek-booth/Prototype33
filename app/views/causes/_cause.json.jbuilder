d67b95fefb1b8917157a98a046e82cd39fb09ba5
Narek-booth <narekmail@mail.ru>
2021-06-04 18:50:31 +0000

    Causes db is ready

---
+++ app/views/causes/_cause.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! cause, :id, :image, :goal, :starts, :ends, :status, :funds_needed, :funds_raised, :commitments_count, :owner_id, :created_at, :updated_at
+json.url cause_url(cause, format: :json)
---
+++ db/migrate/20210604184217_create_causes.rb
@@ -0,0 +1,18 @@
+class CreateCauses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :causes do |t|
+      t.string :image
+      t.string :theme
+      t.text :caption
+      #t.date :start_date
+      #t.date :end_date
+      t.string :status
+      t.integer :funds_needed, default: 0
+      t.integer :funds_raised, default: 0
+      t.integer :commitments_count, default: 0
+      t.belongs_to :owner, null: false, foreign_key: { to_table: :users }, index: true
+
+      t.timestamps
+    end
+  end
+end
