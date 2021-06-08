d7d5f8360a0cd369a55d16b470bbb1af83439fa3
Narek-booth <narekmail@mail.ru>
2021-06-08 11:59:48 +0000

    Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "causes#index"
+  
+  
   resources :posts
   resources :favorites
   resources :commitments
@@ -6,6 +9,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "users#show"
+  
 
 end
